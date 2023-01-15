fb9165e3b97f2b588287e045c1823e57632a0787
xqm32 <458173774@qq.com>
2023-01-15 12:22:56 +0800

    feat: `BlackBox` and `gen_balls()`

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,60 @@
+use std::fmt::Display;
+
+use rand::{rngs::ThreadRng, seq::index::sample, thread_rng};
+
+#[derive(Debug, Clone)]
+enum Grid {
+    Empty,
+    Ball,
+    Ray,
+}
+
+#[derive(Debug)]
+struct BlackBox {
+    rows: usize,
+    cols: usize,
+    grid: Vec<Grid>,
+    rng: ThreadRng,
+}
+
+impl BlackBox {
+    fn new(rows: usize, cols: usize, amount: usize) -> BlackBox {
+        let mut black_box = BlackBox {
+            rows: rows,
+            cols: cols,
+            grid: Vec::with_capacity(rows * cols),
+            rng: thread_rng(),
+        };
+        black_box.grid.resize(rows * cols, Grid::Empty);
+        black_box.gen_balls(amount);
+        black_box
+    }
+
+    fn gen_balls(&mut self, amount: usize) {
+        for i in sample(&mut self.rng, &self.rows * &self.cols, amount).iter() {
+            self.grid[i] = Grid::Ball;
+        }
+    }
+
+    fn get(&self, row: usize, col: usize) -> &Grid {
+        // TODO: 添加越界检查
+        &self.grid[row * self.rows + col]
+    }
+}
+
+impl Display for BlackBox {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        for i in 0..self.rows {
+            for j in 0..self.cols {
+                write!(f, "{:?}\t", self.get(i, j))?
+            }
+            write!(f, "\n")?;
+        }
+        Ok(())
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let black_box = BlackBox::new(4, 4, 4);
+    println!("{}", black_box);
 }
